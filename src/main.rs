d3c6a295e0a938bff3c47bfa94b298fea4802842
melvyn <melvyn2@dnsense.pub>
2020-12-23 18:52:17 -0800

    Day 6 Part 1: Customs

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,18 @@
 
 fn main() {
     let input: Vec<String> = include_str!("input")
-        .split('\n')
-        .map( |s| s.to_string())
+        .split("\n\n")
+        .map( |s| s.replace('\n', "").to_string())
         .collect();
 
-    let mut sid_vec: Vec<u16> = vec!();
-    let mut max_sid = 0;
+    let mut acc = 0;
 
-    for seat in input {
-        let (rowi, coli) = seat.split_at(7);
-
-        let mut row: u16 = 0;
-        for b in rowi.chars() {
-            match b {
-                'F' => {
-                    row = row << 1;
-                }
-                'B' => {
-                    row = (row << 1) | 1;
-                }
-                _ => ()
-            }
-        }
-
-        let mut col: u16 = 0;
-        for b in coli.chars() {
-            match b {
-                'L' => {
-                    col = col << 1;
-                }
-                'R' => {
-                    col = (col << 1) | 1;
-                }
-                _ => ()
-            };
-        }
-
-        let sid = (row * 8) + col;
-        sid_vec.push(sid);
-        if sid > max_sid {
-            max_sid = sid;
-        };
+    for group_raw in input {
+        let mut group = group_raw.into_bytes();
+        group.sort();
+        group.dedup();
+        dbg!(&group.len());
+        acc += group.len();
     }
-
-    // dbg!(sid_vec);
-
-    for sid in &sid_vec {
-        if sid_vec.contains(&(sid + 2)) && ! (sid_vec.contains(&(sid + 1))) {
-            dbg!(sid + 1);
-        }
-    }
+    dbg!(acc);
 }
