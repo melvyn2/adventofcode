1b2cd1a46af579612b55d8a985f8c79f6059936b
melvyn <melvyn2@dnsense.pub>
2020-12-25 16:20:53 -0800

    Day 6 Part 2: Customs (mostly stolen)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 fn main() {
-    let input: Vec<String> = include_str!("input")
+    let answer: usize = include_str!("input")
         .split("\n\n")
-        .map( |s| s.replace('\n', "").to_string())
-        .collect();
+        .map(|group| {
+            group
+            .lines()
+            .map(|person| HashSet::<u8>::from_iter(person.as_bytes().iter().copied()))
+            .fold(HashSet::from_iter("abcdefghijklmnopqrstuvwxyz".to_string().bytes().into_iter()), |acc, p| {
+                acc.intersection(&p).cloned().collect()
+            })
+            .len()
+        })
+        .sum();
 
-    let mut acc = 0;
-
-    for group_raw in input {
-        let mut group = group_raw.into_bytes();
-        group.sort();
-        group.dedup();
-        dbg!(&group.len());
-        acc += group.len();
-    }
-    dbg!(acc);
+    dbg!(answer);
 }
