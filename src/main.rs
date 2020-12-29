d9220e69f9bcbd043af421c2a3cc1ae0396fe63f
melvyn <melvyn2@dnsense.pub>
2020-12-29 10:45:55 -0800

    Day 10 Part 1: Adapters

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,26 @@
 fn main() {
-    let numbers: Vec<usize> = include_str!("input")
+    let mut adapters: Vec<usize> = include_str!("input")
         .split('\n')
         .map( |num| num.parse::<usize>().unwrap() )
         .collect();
+    adapters.sort_unstable();
+    let adapters = adapters;
 
-    // for idx in 0..numbers.len()-25 {
-    //     let preamble:  &[usize] = &numbers[idx..idx+25];
-    //     let num: usize = numbers[idx+25];
-    //     assert_eq!(&preamble.len(), &25usize);
-    //     let mut is_sum: bool = false;
-    //     'first: for first in preamble.iter() {
-    //         for second in preamble.iter() {
-    //             if first == second {
-    //                 continue
-    //             }
-    //             if num == first + second {
-    //                 is_sum = true;
-    //                 break 'first
-    //             }
-    //         }
-    //     }
-    //     if !is_sum {
-    //         dbg!(num);
-    //     }
-    // }
-    let search: usize = 177777905;
-    'outer: for length in 2..100 {
-        for idx in 0..numbers.len()-length {
-            let test: &[usize] = &numbers[idx..idx + length];
-            assert_eq!(test.len(), length);
-            if test.iter().sum::<usize>() == search {
-                dbg!(test.iter().min().unwrap() + test.iter().max().unwrap());
-                break 'outer
-            }
+    let device_rating: usize = adapters.iter().max().unwrap() + 3;
+    dbg!(&adapters, device_rating);
+
+    let mut ch1: usize = 0;
+    let mut ch3: usize = 0;
+    for (idx, adapter) in adapters.iter().enumerate() {
+        if idx == 0 { continue }
+        let change = adapter - adapters[idx-1];
+        if change == 3 {
+            ch3 += 1;
+        } else if change == 1 {
+            ch1 += 1;
+        } else {
+            panic!(change);
         }
     }
+    dbg!(ch1, ch3, (ch1 + 1) * (ch3 + 1));
 }
