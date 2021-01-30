bcf450106426d305dd8f9513e4686c8903d518bc
melvyn <melvyn2@dnsense.pub>
2021-01-30 14:12:06 -0800

    Day 14 part 1: docking & bitmasks

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
-extern crate ring_algorithm;
-use ring_algorithm::chinese_remainder_theorem;
-
 fn main() {
-    let mut lines = include_str!("input").lines();
-    let (idx, buses): (Vec<isize>, Vec<isize>) = lines
-        .nth(1)
-        .unwrap()
-        .split(',')
-        .enumerate()
-        .filter(|(_, x)| *x != "x")
-        .map(|(i, s)| (i as isize, s.parse::<isize>().unwrap()))
-        .unzip();
+    let u36_max: u64 = 68719476735;
+    let ins: Vec<(usize, &str)> = include_str!("input")
+        .lines()
+        .map(|s| {
+            let val = s.split("= ").nth(1).unwrap();
 
-    dbg!(chinese_remainder_theorem(&idx, &buses));
+            let split = s.split_at(4);
+            let addr = if split.0 == "mem[" {
+                assert!(val.parse::<u64>().unwrap() <= u36_max);
+                split.1.split(']').next().unwrap().parse::<usize>().unwrap()
+            } else {
+                assert_eq!(val.len(), 36);
+                0usize
+            };
+            (addr, val)
+        })
+        .collect();
+    let mut sys_mem: Vec<u64> = vec![0u64; ins.iter().map(|x| x.0).max().unwrap() + 1];
+    let mut global_mask: &str = "";
+    for instruction in ins {
+        if instruction.0 == 0 {
+            global_mask = instruction.1
+        } else {
+            let mut val: u64 = instruction.1.parse().unwrap();
+            for char in global_mask.chars().enumerate() {
+                match char.1 {
+                    'X' => continue,
+                    '1' => val |= 1 << (35 - char.0),
+                    '0' => val &= !(1 << (35 - char.0)),
+                    _ => unreachable!(),
+                }
+            }
+            sys_mem[instruction.0] = val;
+        }
+    }
+    dbg!(sys_mem.iter().sum::<u64>());
 }
