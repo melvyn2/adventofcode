69c8b72d44cb56ec508cd49596844cf197103d32
melvyn <melvyn2@dnsense.pub>
2021-02-04 18:34:30 -0800

    Day 16 part 1: tickets

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,57 @@
-use std::collections::HashMap;
+use std::ops::RangeInclusive;
+
+// Can't create impl for foreign type (E0117)
+fn range_from_str(s: &str) -> RangeInclusive<usize> {
+    let mut split = s.split('-');
+    let lhs = split.next().unwrap().parse::<usize>().unwrap();
+    let rhs = split.next().unwrap().parse::<usize>().unwrap();
+    lhs..=rhs
+}
 
 fn main() {
-    let target: usize = 30000000;
-    let input: Vec<usize> = vec![11, 0, 1, 10, 5, 19];
-    let (input, last) = input.split_at(input.len() - 1);
-    let mut last = *last.first().unwrap();
-    let mut spoken: HashMap<usize, usize> = input
-        .iter()
-        .enumerate()
-        .map(|(i, x)| (*x as usize, i))
+    let mut input = include_str!("input").split("\n\n");
+    let constraints: Vec<(RangeInclusive<usize>, RangeInclusive<usize>)> = input
+        .next()
+        .unwrap()
+        .lines()
+        .map(|s| {
+            let mut split = s.split(' ').rev();
+            let r2: RangeInclusive<usize> = range_from_str(split.next().unwrap());
+            let r1: RangeInclusive<usize> = range_from_str(split.nth(1).unwrap());
+            (r1, r2)
+        })
         .collect();
-    for idx in (&spoken).len()..target - 1 {
-        let res = match spoken.get(&last) {
-            Some(second_last_idx) => idx - second_last_idx,
-            None => 0,
-        };
-        spoken.insert(last, idx);
-        last = res;
+    let self_ticket: Vec<usize> = input
+        .next()
+        .unwrap()
+        .lines()
+        .nth(1)
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+    let other_tickets: Vec<Vec<usize>> = input
+        .next()
+        .unwrap()
+        .lines()
+        .skip(1)
+        .map(|x| {
+            x.split(',')
+                .map(|s| s.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect();
+    let mut erate: Vec<usize> = vec![];
+    'val: for val in other_tickets.iter().flatten() {
+        for constraint_pair in &constraints {
+            if constraint_pair.0.contains(val) {
+                continue 'val;
+            }
+            if constraint_pair.1.contains(val) {
+                continue 'val;
+            }
+        }
+        erate.push(*val);
     }
-    dbg!(last);
+    dbg!(erate.iter().sum::<usize>());
 }
