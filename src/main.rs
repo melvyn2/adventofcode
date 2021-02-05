523dc55699ed758972bbe48ac1338c1f7e658aee
melvyn <melvyn2@dnsense.pub>
2021-02-04 16:07:27 -0800

    Day 15 part 1: memory game

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,18 @@
-use std::collections::HashMap;
-
 fn main() {
-    let u36_max: u64 = 68719476735;
-    let ins: Vec<(usize, &str)> = include_str!("input")
-        .lines()
-        .map(|s| {
-            let val = s.split("= ").nth(1).unwrap();
-
-            let split = s.split_at(4);
-            let addr = if split.0 == "mem[" {
-                assert!(val.parse::<u64>().unwrap() <= u36_max);
-                split.1.split(']').next().unwrap().parse::<usize>().unwrap()
-            } else {
-                assert_eq!(val.len(), 36);
-                0usize
-            };
-            (addr, val)
-        })
-        .collect();
-    let mut sys_mem: HashMap<usize, u64> = HashMap::new();
-    let mut global_mask = "".chars().rev().enumerate();
-    for instruction in ins {
-        if instruction.0 == 0 {
-            global_mask = instruction.1.chars().rev().enumerate();
+    let mut spoken: Vec<usize> = vec![11, 0, 1, 10, 5, 19];
+    for idx in (&spoken).len()..2020 {
+        let prev = spoken.clone();
+        let a: Vec<(usize, &usize)> = prev
+            .iter()
+            .enumerate()
+            .rev()
+            .filter(|(_, &x)| x == prev[idx - 1])
+            .collect();
+        if a.len() > 1usize {
+            spoken.push(a[0].0 - a[1].0);
         } else {
-            let mut addrs: Vec<usize> = vec![instruction.0];
-            for index in global_mask
-                .clone()
-                .filter(|(_, x)| *x == '1')
-                .map(|(idx, _)| idx)
-            {
-                addrs[0] |= 1 << index;
-            }
-            for index in global_mask
-                .clone()
-                .filter(|(_, x)| *x == 'X')
-                .map(|(idx, _)| idx)
-            {
-                for addr in addrs.clone() {
-                    addrs.push(addr ^ (1 << index))
-                }
-            }
-
-            sys_mem.extend(
-                addrs
-                    .iter()
-                    .map(|a| (*a, instruction.1.parse::<u64>().unwrap())),
-            );
+            spoken.push(0);
         }
     }
-    dbg!(sys_mem.values().sum::<u64>());
+    dbg!(spoken[2020 - 1]);
 }
