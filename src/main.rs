ae4f4fe6e22c7139460d733711e4814d207af497
melvyn <melvyn2@dnsense.pub>
2021-02-04 17:35:01 -0800

    Day 15 part 2: memory game (second pass)

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
+use std::collections::HashMap;
+
 fn main() {
-    let target: f64 = 30000f64;
-    let mut spoken: Vec<usize> = vec![0, 3, 6];
-    for idx in (&spoken).len()..target as usize {
-        let mut a = spoken
-            .iter()
-            .enumerate()
-            .rev()
-            .skip(1)
-            .filter(|(_, &x)| x == spoken[idx - 1]);
-        let n = a.next();
-        let res: usize = match n {
-            Some((second_idx, _)) => (idx - 1) - second_idx,
+    let target: usize = 30000000;
+    let input: Vec<usize> = vec![11, 0, 1, 10, 5, 19];
+    let (input, last) = input.split_at(input.len() - 1);
+    let mut last = *last.first().unwrap();
+    let mut spoken: HashMap<usize, usize> = input
+        .iter()
+        .enumerate()
+        .map(|(i, x)| (*x as usize, i))
+        .collect();
+    for idx in (&spoken).len()..target - 1 {
+        let res = match spoken.get(&last) {
+            Some(second_last_idx) => idx - second_last_idx,
             None => 0,
         };
-        spoken.push(res);
-        // print!("\r {}", (idx as f64 / target));
+        spoken.insert(last, idx);
+        last = res;
     }
-    println!();
-    // dbg!(&spoken);
-    dbg!(spoken[target as usize - 1]);
+    dbg!(last);
 }
