2a7c1d08e7df21d2bc17f917050dfb382aa1d675
melvyn <melvyn2@dnsense.pub>
2021-02-04 17:06:16 -0800

    Day 15 part 2: memory game (first pass)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 fn main() {
-    let target: f64 = 30000000f64;
+    let target: f64 = 30000f64;
     let mut spoken: Vec<usize> = vec![0, 3, 6];
     for idx in (&spoken).len()..target as usize {
-        let prev = spoken.clone();
-        let a: Vec<(usize, &usize)> = prev
+        let mut a = spoken
             .iter()
             .enumerate()
             .rev()
-            .filter(|(_, &x)| x == prev[idx - 1])
-            .collect();
-        if a.len() > 1usize {
-            spoken.push(a[0].0 - a[1].0);
-        } else {
-            spoken.push(0);
-        }
-        print!("\r {}", idx as f64 / target)
+            .skip(1)
+            .filter(|(_, &x)| x == spoken[idx - 1]);
+        let n = a.next();
+        let res: usize = match n {
+            Some((second_idx, _)) => (idx - 1) - second_idx,
+            None => 0,
+        };
+        spoken.push(res);
+        // print!("\r {}", (idx as f64 / target));
     }
+    println!();
+    // dbg!(&spoken);
     dbg!(spoken[target as usize - 1]);
 }
