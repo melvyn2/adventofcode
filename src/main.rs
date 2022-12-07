12dffd65ad4a8de9e94ae23e95c0df7220fd2173
melvyn2 <melvyn2@brcok.tk>
2022-12-06 18:44:02 -0800

    Day 6 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,22 @@
+use std::collections::HashSet;
+
 fn main() {
-    let mut input_s = include_str!("input.txt").split("\n\n");
-    let mut stacks: Vec<Vec<char>> = vec![vec![]; 9]; // Set manually
-    for line in input_s.next().unwrap().lines() {
-        for (idx, c) in line
-            .chars()
-            .enumerate()
-            .filter(|(_, c)| c.is_ascii_alphabetic())
+    let input = include_str!("input.txt");
+    for c in input
+        .chars()
+        .enumerate()
+        .collect::<Vec<(usize, char)>>()
+        .windows(4)
+    {
+        if c.iter()
+            .cloned()
+            .map(|(_, c)| c)
+            .collect::<HashSet<char>>()
+            .len()
+            == c.len()
         {
-            stacks[idx / 4].insert(0, c);
+            dbg!(c.last().unwrap().0 + 1);
+            break;
         }
     }
-    let mut moves: Vec<(usize, usize, usize)> = vec![];
-    for line in input_s.next().unwrap().lines() {
-        let mut words = line.split_whitespace();
-        let ct = words.nth(1).unwrap().parse::<usize>().unwrap();
-        let src = words.nth(1).unwrap().parse::<usize>().unwrap();
-        let dest = words.nth(1).unwrap().parse::<usize>().unwrap();
-        moves.push((src, dest, ct));
-    }
-    for (src, dest, ct) in moves {
-        let src_range = stacks[src - 1].len() - ct..stacks[src - 1].len();
-        let mut itms = stacks[src - 1].drain(src_range).collect();
-        stacks[dest - 1].append(&mut itms);
-    }
-    dbg!(stacks
-        .iter()
-        .map(|s| *s.last().unwrap())
-        .collect::<String>());
 }
