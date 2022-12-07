2a527f246de55c9786eafcdfab085a4bd00a0bfe
melvyn2 <melvyn2@brcok.tk>
2022-12-06 18:31:57 -0800

    Day 5 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
 fn main() {
-    let input = include_str!("input.txt");
-    let items: u32 = input
-        .lines()
-        .map(|line| {
-            let mut split = line.split(',');
-            let (mut r1s, mut r2s) = (
-                split.next().unwrap().split('-'),
-                split.next().unwrap().split('-'),
-            );
-            let r1 = r1s.next().unwrap().parse::<u8>().unwrap()
-                ..=r1s.next().unwrap().parse::<u8>().unwrap();
-            let r2 = r2s.next().unwrap().parse::<u8>().unwrap()
-                ..=r2s.next().unwrap().parse::<u8>().unwrap();
-
-            (r1.contains(r2.start()) || r1.contains(r2.end()))
-                || (r2.contains(r1.start()) || r2.contains(r1.end()))
-        })
-        .map(|b| b as u32)
-        .sum();
-    dbg!(items);
+    let mut input_s = include_str!("input.txt").split("\n\n");
+    let mut stacks: Vec<Vec<char>> = vec![vec![]; 9]; // Set manually
+    for line in input_s.next().unwrap().lines() {
+        for (idx, c) in line
+            .chars()
+            .enumerate()
+            .filter(|(_, c)| c.is_ascii_alphabetic())
+        {
+            stacks[idx / 4].insert(0, c);
+        }
+    }
+    let mut moves: Vec<(usize, usize)> = vec![];
+    for line in input_s.next().unwrap().lines() {
+        let mut words = line.split_whitespace();
+        let ct = words.nth(1).unwrap().parse::<usize>().unwrap();
+        let src = words.nth(1).unwrap().parse::<usize>().unwrap();
+        let dest = words.nth(1).unwrap().parse::<usize>().unwrap();
+        for _ in 0..ct {
+            moves.push((src, dest));
+        }
+    }
+    for (src, dest) in moves {
+        let i = stacks[src - 1].pop().unwrap();
+        stacks[dest - 1].push(i);
+    }
+    dbg!(stacks
+        .iter()
+        .map(|s| s.last().unwrap().clone())
+        .collect::<String>());
 }
