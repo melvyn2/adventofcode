af37086d5e76ab92d6ee676a95b8136f5081fee4
melvyn2 <melvyn2@brcok.tk>
2022-12-08 11:09:30 -0800

    Day 8 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,38 @@
-#![feature(iter_advance_by)]
-
-use std::collections::HashMap;
-use std::path::PathBuf;
-
 fn main() {
-    let mut cmds = include_str!("input.txt").split("\n$ ");
-    cmds.advance_by(1).unwrap();
-    let mut pwd = PathBuf::from("/");
-    let mut fs: HashMap<String, Vec<(&str, usize)>> = HashMap::new();
-    for cmd in cmds {
-        let (cmd_base, rest) = cmd.split_at(3);
-        let mut lines = rest.lines();
-        match cmd_base {
-            "cd " => {
-                let path = lines.next().unwrap();
-                if path == ".." {
-                    pwd.pop();
-                } else {
-                    pwd.push(path);
-                }
-                // dbg!(&pwd);
-            }
-            "ls\n" => {
-                let contents = lines
-                    .filter(|s| !s.starts_with("dir"))
-                    .map(|l| {
-                        let (size_s, name) = l.split_once(" ").unwrap();
-                        (name, size_s.parse::<usize>().unwrap())
-                    })
-                    .collect::<Vec<(&str, usize)>>();
-                fs.insert(pwd.to_string_lossy().to_string(), contents);
-                // dbg!(&fs[pwd.to_str().unwrap()]);
-            }
-            _ => unreachable!(),
-        }
-    }
-
-    let size_to_free = 30000000
-        - (70000000
-            - fs.values()
-                .map(|files| files.iter().map(|(_, s)| s).sum::<usize>())
-                .sum::<usize>());
-
-    let min = fs
-        .keys()
-        .clone()
-        .map(|dir| {
-            fs.iter()
-                .filter(|(d, _)| d.starts_with(dir))
-                .map(|(_, files)| files.iter().map(|(_, s)| s).sum::<usize>())
-                .sum::<usize>()
+    let input = include_str!("input.txt");
+    let trees: Vec<Vec<u8>> = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap() as u8)
+                .collect::<Vec<u8>>()
         })
-        .filter(|&s| s >= size_to_free)
-        .min()
-        .unwrap();
-
-    dbg!(min);
+        .collect();
+    let res = trees
+        .clone()
+        .iter()
+        .enumerate()
+        .map(|(r_idx, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(c_idx, tree)| {
+                    if ((r_idx == 0 || r_idx == trees.len() - 1)
+                        || (c_idx == 0 || c_idx == row.len() - 1))
+                        || ((tree > row[..c_idx].iter().max().unwrap())
+                            || (tree > row[c_idx + 1..].iter().max().unwrap())
+                            || (tree > &trees[..r_idx].iter().map(|r| r[c_idx]).max().unwrap())
+                            || (tree > &trees[r_idx + 1..].iter().map(|r| r[c_idx]).max().unwrap()))
+                    {
+                        // dbg!(tree);
+                        return 1;
+                    }
+                    return 0;
+                })
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>();
+    for row in &res {
+        println!("{:?}", row);
+    }
+    dbg!(res.iter().flatten().sum::<u32>());
 }
