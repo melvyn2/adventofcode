91da8e3bb17ae02ba4787f84d744d28a5e147b07
melvyn2 <melvyn2@brcok.tk>
2022-12-07 16:49:47 -0800

    Day 7 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,52 @@
-use std::collections::HashSet;
+#![feature(iter_advance_by)]
+
+use std::collections::HashMap;
+use std::path::PathBuf;
 
 fn main() {
-    let input = include_str!("input.txt");
-    for c in input
-        .chars()
-        .enumerate()
-        .collect::<Vec<(usize, char)>>()
-        .windows(14)
-    {
-        if c.iter()
-            .cloned()
-            .map(|(_, c)| c)
-            .collect::<HashSet<char>>()
-            .len()
-            == c.len()
-        {
-            dbg!(c.last().unwrap().0 + 1);
-            break;
+    let mut cmds = include_str!("input.txt").split("\n$ ");
+    cmds.advance_by(1).unwrap();
+    let mut pwd = PathBuf::from("/");
+    let mut fs: HashMap<String, Vec<(&str, usize)>> = HashMap::new();
+    for cmd in cmds {
+        let (cmd_base, rest) = cmd.split_at(3);
+        let mut lines = rest.lines();
+        match cmd_base {
+            "cd " => {
+                let path = lines.next().unwrap();
+                if path == ".." {
+                    pwd.pop();
+                } else {
+                    pwd.push(path);
+                }
+                // dbg!(&pwd);
+            }
+            "ls\n" => {
+                let contents = lines
+                    .filter(|s| !s.starts_with("dir"))
+                    .map(|l| {
+                        let (size_s, name) = l.split_once(" ").unwrap();
+                        (name, size_s.parse::<usize>().unwrap())
+                    })
+                    .collect::<Vec<(&str, usize)>>();
+                fs.insert(pwd.to_string_lossy().to_string(), contents);
+                // dbg!(&fs[pwd.to_str().unwrap()]);
+            }
+            _ => unreachable!(),
         }
     }
+
+    let sum = fs
+        .keys()
+        .clone()
+        .map(|dir| {
+            fs.iter()
+                .filter(|(d, _)| d.starts_with(dir))
+                .map(|(_, files)| files.iter().map(|(_, s)| s).sum::<usize>())
+                .sum::<usize>()
+        })
+        .filter(|&s| s <= 100000)
+        .sum::<usize>();
+
+    dbg!(sum);
 }
