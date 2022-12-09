4d50d48c86b142db71fec280f0aaa70b80ae84ed
melvyn2 <melvyn2@brcok.tk>
2022-12-08 17:10:35 -0800

    Day 8 Part 2

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,52 @@ fn main() {
             row.iter()
                 .enumerate()
                 .map(|(c_idx, tree)| {
-                    if ((r_idx == 0 || r_idx == trees.len() - 1)
-                        || (c_idx == 0 || c_idx == row.len() - 1))
-                        || ((tree > row[..c_idx].iter().max().unwrap())
-                            || (tree > row[c_idx + 1..].iter().max().unwrap())
-                            || (tree > &trees[..r_idx].iter().map(|r| r[c_idx]).max().unwrap())
-                            || (tree > &trees[r_idx + 1..].iter().map(|r| r[c_idx]).max().unwrap()))
+                    if (r_idx == 0 || r_idx == trees.len() - 1)
+                        || (c_idx == 0 || c_idx == row.len() - 1)
                     {
-                        // dbg!(tree);
-                        return 1;
+                        return 0;
                     }
-                    return 0;
+                    let r_vd = row[c_idx + 1..]
+                        .iter()
+                        .enumerate()
+                        .find(|(_, t)| t >= &tree)
+                        .unwrap_or((row[c_idx + 1..].len() - 1, &0))
+                        .0
+                        + 1;
+                    let l_vd = row[..c_idx]
+                        .iter()
+                        .rev()
+                        .enumerate()
+                        .find(|(_, t)| t >= &tree)
+                        .unwrap_or((row[..c_idx].len() - 1, &0))
+                        .0
+                        + 1;
+                    let col = trees.iter().map(|r| r[c_idx]).collect::<Vec<u8>>();
+                    let u_vd = col[..r_idx]
+                        .iter()
+                        .rev()
+                        .enumerate()
+                        .find(|(_, t)| t >= &tree)
+                        .unwrap_or((col[..r_idx].len() - 1, &0))
+                        .0
+                        + 1;
+                    let d_vd = col[r_idx + 1..]
+                        .iter()
+                        .enumerate()
+                        .find(|(_, t)| t >= &tree)
+                        .unwrap_or((col[r_idx + 1..].len() - 1, &0))
+                        .0
+                        + 1;
+
+                    l_vd * r_vd * d_vd * u_vd
                 })
-                .collect::<Vec<u32>>()
+                .collect::<Vec<usize>>()
         })
-        .collect::<Vec<Vec<u32>>>();
+        .collect::<Vec<Vec<usize>>>();
+
     for row in &res {
         println!("{:?}", row);
     }
-    dbg!(res.iter().flatten().sum::<u32>());
+
+    dbg!(res.iter().flatten().max());
 }
