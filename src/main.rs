4e5e998f80e1c270e9c282855c3cbcea21812a1b
melvyn2 <melvyn2@brcok.tk>
2022-12-13 12:09:23 -0800

    Day 10 Part 2

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,25 @@ fn main() {
     let input = include_str!("input.txt");
     let mut reg_x: i32 = 1;
     let mut cyc: u32 = 0;
-    let mut acc: u32 = 0;
+    let mut crt: Vec<Vec<char>> = vec![vec![' '; 40]; 6];
     for inst_str in input.lines() {
-        cyc += 1;
-        if cyc % 40 == 20 {
-            acc += cyc * reg_x as u32;
+        let crt_x: usize = (cyc % 40) as usize;
+        let crt_y: usize = (cyc / 40) as usize;
+        if (reg_x - 1..=reg_x + 1).contains(&(crt_x as i32)) {
+            crt[crt_y][crt_x] = '#';
         }
+        cyc += 1;
         if let Some((_, add_s)) = inst_str.split_once(' ') {
-            cyc += 1;
-            if cyc % 40 == 20 {
-                acc += cyc * reg_x as u32;
+            let crt_x: usize = (cyc % 40) as usize;
+            let crt_y: usize = (cyc / 40) as usize;
+            if (reg_x - 1..=reg_x + 1).contains(&(crt_x as i32)) {
+                crt[crt_y][crt_x] = '#';
             }
+            cyc += 1;
             reg_x += add_s.parse::<i32>().unwrap();
         };
     }
-    dbg!(acc);
+    for line in crt {
+        println!("{}", line.iter().collect::<String>());
+    }
 }
