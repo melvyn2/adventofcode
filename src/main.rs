0e4749483bd06330e9ff346107622c83780dd7ca
melvyn2 <melvyn2@brcok.tk>
2022-12-15 11:01:15 -0800

    Day 9 Part 2

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,41 @@ use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("input.txt");
-    let mut head: (i32, i32) = (0, 0);
-    let mut tail: (i32, i32) = (0, 0);
-    let mut pos_set: HashSet<(i32, i32)> = HashSet::from([tail]);
+    let mut knots = vec![(0, 0); 10];
+    let mut pos_set: HashSet<(i32, i32)> = HashSet::from([knots[9]]);
     for (dir_s, num_s) in input.lines().map(|s| s.split_once(' ').unwrap()) {
         let num = num_s.parse::<i32>().unwrap();
         println!("{}  {}", dir_s, num);
         for _ in 0..num {
             match dir_s.chars().next().unwrap() {
                 'L' => {
-                    head.0 -= 1;
+                    knots[0].0 -= 1;
                 }
                 'R' => {
-                    head.0 += 1;
+                    knots[0].0 += 1;
                 }
                 'U' => {
-                    head.1 -= 1;
+                    knots[0].1 -= 1;
                 }
                 'D' => {
-                    head.1 += 1;
+                    knots[0].1 += 1;
                 }
                 _ => unreachable!(),
             }
-            let diff = (head.0 - tail.0, head.1 - tail.1);
-            if diff.0.abs() + diff.1.abs() > 2 {
-                tail.0 += diff.0 / diff.0.abs();
-                tail.1 += diff.1 / diff.1.abs();
-            } else if diff.0.abs() > 1 {
-                tail.0 += diff.0 / diff.0.abs();
-            } else if diff.1.abs() > 1 {
-                tail.1 += diff.1 / diff.1.abs();
-            }
-            pos_set.insert(tail);
-        }
-        for yp in -5..=5 {
-            for xp in -5..=5 {
-                if (xp, yp) == head {
-                    print!("H");
-                } else if (xp, yp) == tail {
-                    print!("T")
-                } else if (xp, yp) == (0, 0) {
-                    print!("s");
-                } else {
-                    print!(".");
+            for head_idx in 0..9 {
+                let mut tail = knots[head_idx + 1];
+                let diff = (knots[head_idx].0 - tail.0, knots[head_idx].1 - tail.1);
+                if diff.0.abs() + diff.1.abs() > 2 {
+                    tail.0 += diff.0 / diff.0.abs();
+                    tail.1 += diff.1 / diff.1.abs();
+                } else if diff.0.abs() > 1 {
+                    tail.0 += diff.0 / diff.0.abs();
+                } else if diff.1.abs() > 1 {
+                    tail.1 += diff.1 / diff.1.abs();
                 }
+                knots[head_idx + 1] = tail;
             }
-            println!();
+            pos_set.insert(knots[9]);
         }
     }
     dbg!(pos_set.len());
