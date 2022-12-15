fa4dfdf56f3e6243f767271eff09fa51daa34990
melvyn2 <melvyn2@brcok.tk>
2022-12-15 11:01:01 -0800

    Day 9 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("input.txt");
-    let mut reg_x: i32 = 1;
-    let mut cyc: u32 = 0;
-    let mut crt: Vec<Vec<char>> = vec![vec![' '; 40]; 6];
-    for inst_str in input.lines() {
-        let crt_x: usize = (cyc % 40) as usize;
-        let crt_y: usize = (cyc / 40) as usize;
-        if (reg_x - 1..=reg_x + 1).contains(&(crt_x as i32)) {
-            crt[crt_y][crt_x] = '#';
-        }
-        cyc += 1;
-        if let Some((_, add_s)) = inst_str.split_once(' ') {
-            let crt_x: usize = (cyc % 40) as usize;
-            let crt_y: usize = (cyc / 40) as usize;
-            if (reg_x - 1..=reg_x + 1).contains(&(crt_x as i32)) {
-                crt[crt_y][crt_x] = '#';
+    let mut head: (i32, i32) = (0, 0);
+    let mut tail: (i32, i32) = (0, 0);
+    let mut pos_set: HashSet<(i32, i32)> = HashSet::from([tail]);
+    for (dir_s, num_s) in input.lines().map(|s| s.split_once(' ').unwrap()) {
+        let num = num_s.parse::<i32>().unwrap();
+        println!("{}  {}", dir_s, num);
+        for _ in 0..num {
+            match dir_s.chars().next().unwrap() {
+                'L' => {
+                    head.0 -= 1;
+                }
+                'R' => {
+                    head.0 += 1;
+                }
+                'U' => {
+                    head.1 -= 1;
+                }
+                'D' => {
+                    head.1 += 1;
+                }
+                _ => unreachable!(),
             }
-            cyc += 1;
-            reg_x += add_s.parse::<i32>().unwrap();
-        };
+            let diff = (head.0 - tail.0, head.1 - tail.1);
+            if diff.0.abs() + diff.1.abs() > 2 {
+                tail.0 += diff.0 / diff.0.abs();
+                tail.1 += diff.1 / diff.1.abs();
+            } else if diff.0.abs() > 1 {
+                tail.0 += diff.0 / diff.0.abs();
+            } else if diff.1.abs() > 1 {
+                tail.1 += diff.1 / diff.1.abs();
+            }
+            pos_set.insert(tail);
+        }
+        for yp in -5..=5 {
+            for xp in -5..=5 {
+                if (xp, yp) == head {
+                    print!("H");
+                } else if (xp, yp) == tail {
+                    print!("T")
+                } else if (xp, yp) == (0, 0) {
+                    print!("s");
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
+        }
     }
-    for line in crt {
-        println!("{}", line.iter().collect::<String>());
-    }
+    dbg!(pos_set.len());
 }
