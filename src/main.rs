c072d4448090321c35213537e7f08c729dc34ea9
melvyn <melvyn2@dnsense.pub>
2023-12-05 20:14:39 -0800

    Day 1 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,46 @@
+fn str_to_digits(s: &[u8]) -> u32 {
+    let digits = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]
+    .map(|s| s.as_bytes());
+
+    let mut first: u32 = 11;
+    'outer: for i in 0..s.len() {
+        if (s[i] as char).is_numeric() {
+            first = (s[i] as char).to_digit(10).unwrap();
+            break 'outer;
+        }
+        for (n, &d) in digits.iter().enumerate() {
+            if i + d.len() <= s.len() && &s[i..i + d.len()] == d {
+                first = n as u32;
+                break 'outer;
+            }
+        }
+    }
+
+    let mut last: u32 = 11;
+    // don't just copy paste .-.
+    'outer: for i in (0..s.len()).rev() {
+        if (s[i] as char).is_numeric() {
+            last = (s[i] as char).to_digit(10).unwrap();
+            break 'outer;
+        }
+        for (n, &d) in digits.iter().enumerate() {
+            if i + d.len() <= s.len() && &s[i..i + d.len()] == d {
+                last = n as u32;
+                break 'outer;
+            }
+        }
+    }
+
+    // Make sure we actually got digits
+    assert!(first < 10 && last < 10);
+
+    first * 10 + last
+}
+
 fn main() {
     let input = include_str!("input.txt");
-    let acc: u32 = input
-        .lines()
-        .map(|l| {
-            let mut nums = l.chars().filter(|c| c.is_numeric());
-            let first = nums.next().and_then(|c| c.to_digit(10)).unwrap();
-            first * 10 + nums.last().and_then(|c| c.to_digit(10)).unwrap_or(first)
-        })
-        .sum();
+    let acc: u32 = input.lines().map(|l| str_to_digits(l.as_bytes())).sum();
     dbg!(acc);
 }
