fae1121f11ff30bfa0cd3e6262b5902d137d9007
melvyn <melvyn2@dnsense.pub>
2023-12-05 20:30:45 -0800

    Day 2 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
+use std::cmp::max;
+
 fn main() {
     let input = include_str!("input.txt");
 
     let mut acc = 0;
-    'game: for (idx, game) in input.lines().enumerate() {
+    for game in input.lines() {
+        let mut min_colors = (0, 0, 0);
         let draws = game.split_once(": ").unwrap().1.split("; ");
         for draw in draws {
-            let (mut red, mut green, mut blue) = (0, 0, 0);
+            let mut colors = (0, 0, 0);
             for color_draw in draw.split(", ") {
                 let (count, color) = color_draw.split_once(' ').unwrap();
                 let n = count.parse::<u32>().unwrap();
                 match color {
-                    "red" => red += n,
-                    "green" => green += n,
-                    "blue" => blue += n,
+                    "red" => colors.0 += n,
+                    "green" => colors.1 += n,
+                    "blue" => colors.2 += n,
                     _ => unreachable!(),
                 }
             }
-            if red > 12 || green > 13 || blue > 14 {
-                continue 'game;
-            }
+            min_colors.0 = max(min_colors.0, colors.0);
+            min_colors.1 = max(min_colors.1, colors.1);
+            min_colors.2 = max(min_colors.2, colors.2);
         }
-        acc += idx + 1;
+        acc += min_colors.0 * min_colors.1 * min_colors.2;
     }
     dbg!(acc);
 }
