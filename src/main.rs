cfca174fba2b2bd9ee688786c34244a20d1c0772
melvyn <melvyn2@dnsense.pub>
2023-12-05 20:27:12 -0800

    Day 2 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,26 @@
-fn str_to_digits(s: &[u8]) -> u32 {
-    let digits = [
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ]
-    .map(|s| s.as_bytes());
-
-    let mut first: u32 = 11;
-    'outer: for i in 0..s.len() {
-        if (s[i] as char).is_numeric() {
-            first = (s[i] as char).to_digit(10).unwrap();
-            break 'outer;
-        }
-        for (n, &d) in digits.iter().enumerate() {
-            if i + d.len() <= s.len() && &s[i..i + d.len()] == d {
-                first = n as u32;
-                break 'outer;
-            }
-        }
-    }
-
-    let mut last: u32 = 11;
-    // don't just copy paste .-.
-    'outer: for i in (0..s.len()).rev() {
-        if (s[i] as char).is_numeric() {
-            last = (s[i] as char).to_digit(10).unwrap();
-            break 'outer;
-        }
-        for (n, &d) in digits.iter().enumerate() {
-            if i + d.len() <= s.len() && &s[i..i + d.len()] == d {
-                last = n as u32;
-                break 'outer;
-            }
-        }
-    }
-
-    // Make sure we actually got digits
-    assert!(first < 10 && last < 10);
-
-    first * 10 + last
-}
-
 fn main() {
     let input = include_str!("input.txt");
-    let acc: u32 = input.lines().map(|l| str_to_digits(l.as_bytes())).sum();
+
+    let mut acc = 0;
+    'game: for (idx, game) in input.lines().enumerate() {
+        let draws = game.split_once(": ").unwrap().1.split("; ");
+        for draw in draws {
+            let (mut red, mut green, mut blue) = (0, 0, 0);
+            for color_draw in draw.split(", ") {
+                let (count, color) = color_draw.split_once(' ').unwrap();
+                let n = count.parse::<u32>().unwrap();
+                match color {
+                    "red" => red += n,
+                    "green" => green += n,
+                    "blue" => blue += n,
+                    _ => unreachable!(),
+                }
+            }
+            if red > 12 || green > 13 || blue > 14 {
+                continue 'game;
+            }
+        }
+        acc += idx + 1;
+    }
     dbg!(acc);
 }
