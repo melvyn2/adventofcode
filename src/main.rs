3ce9eeb47e05b80ed35cfa8c39291fadd9ff13c1
melvyn <melvyn2@dnsense.pub>
2023-12-08 15:23:37 -0800

    Day 4 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
     let input = include_str!("input.txt");
 
-    let acc = input
+    let map = input
         .lines()
         .map(|l| {
-            let nums = l.split_once(": ").unwrap().1;
+            let (_game_str, nums) = l.split_once(": ").unwrap();
+
             let (wins_str, given_str) = nums.split_once(" | ").unwrap();
-            let wins = wins_str
+            let winning = wins_str
                 .split(' ')
                 .filter(|&s| !s.is_empty())
                 .map(|n| n.parse().unwrap())
@@ -16,13 +17,25 @@ fn main() {
                 .filter(|&s| !s.is_empty())
                 .map(|n| n.parse().unwrap())
                 .collect::<Vec<u32>>();
-            let count = given.iter().filter(|&n| wins.contains(n)).count();
-            if count == 0 {
-                0
-            } else {
-                2u32.pow(count as u32 - 1)
-            }
+            let wins = given.iter().filter(|&n| winning.contains(n)).count();
+            wins
         })
-        .sum::<u32>();
+        .collect::<Vec<usize>>();
+
+    let mut acc = 0;
+    let mut cur = vec![1; map.len()];
+    let mut next = vec![0; map.len()];
+
+    while cur.iter().sum::<usize>() > 0 {
+        for (game, &wins) in map.iter().enumerate() {
+            acc += cur[game];
+            for i in next.iter_mut().skip(game + 1).take(wins) {
+                *i += cur[game];
+            }
+        }
+        cur = next;
+        next = vec![0; map.len()];
+    }
+
     dbg!(acc);
 }
