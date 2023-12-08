ad497f14765af75f6a7173356311976d9e6992a8
melvyn <melvyn2@dnsense.pub>
2023-12-08 13:06:10 -0800

    Day 3 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,91 @@
-use std::cmp::max;
+use std::collections::BTreeSet;
+
+fn nudge_left_numeric(mut pos: usize, plane: &[u8], line_len: usize) -> usize {
+    while (pos % line_len) > 0 && (plane[pos - 1] as char).is_numeric() {
+        pos -= 1;
+    }
+    pos
+}
+
+fn expand_right_and_collect_numeric(pos: usize, plane: &[u8], line_len: usize) -> usize {
+    let mut num = (plane[pos] as char).to_digit(10).unwrap();
+    let mut len = 1;
+    while ((pos % line_len) + len) <= line_len && (plane[pos + len] as char).is_numeric() {
+        num = (num * 10) + (plane[pos + len] as char).to_digit(10).unwrap();
+        len += 1;
+    }
+    num as usize
+}
+
+fn find_numeric(pos: usize, plane: &[u8], line_len: usize, set: &mut BTreeSet<usize>) -> bool {
+    let mut acc = false;
+
+    let x = pos % line_len;
+    let y = pos / line_len;
+    let xleft = x > 0;
+    let xright = x < line_len - 1;
+    if y > 0 {
+        if xleft && (plane[pos - (line_len + 1)] as char).is_numeric() {
+            acc |= set.insert(nudge_left_numeric(pos - (line_len + 1), plane, line_len));
+        }
+        if (plane[pos - line_len] as char).is_numeric() {
+            acc |= set.insert(nudge_left_numeric(pos - (line_len), plane, line_len));
+        }
+        if xright && (plane[pos - (line_len - 1)] as char).is_numeric() {
+            acc |= set.insert(nudge_left_numeric(pos - (line_len - 1), plane, line_len));
+        }
+    }
+    if y < (plane.len() / line_len) - 1 {
+        if xleft && (plane[pos + (line_len - 1)] as char).is_numeric() {
+            acc |= set.insert(nudge_left_numeric(pos + (line_len - 1), plane, line_len));
+        }
+        if (plane[pos + line_len] as char).is_numeric() {
+            acc |= set.insert(nudge_left_numeric(pos + (line_len), plane, line_len));
+        }
+        if xright && (plane[pos + (line_len + 1)] as char).is_numeric() {
+            acc |= set.insert(nudge_left_numeric(pos + (line_len + 1), plane, line_len));
+        }
+    }
+    if xleft && (plane[pos - 1] as char).is_numeric() {
+        acc |= set.insert(nudge_left_numeric(pos - 1, plane, line_len));
+    }
+    if xright && (plane[pos + 1] as char).is_numeric() {
+        acc |= set.insert(nudge_left_numeric(pos + 1, plane, line_len));
+    }
+
+    acc
+}
 
 fn main() {
-    let input = include_str!("input.txt");
+    let input = include_bytes!("input.txt");
 
-    let mut acc = 0;
-    for game in input.lines() {
-        let mut min_colors = (0, 0, 0);
-        let draws = game.split_once(": ").unwrap().1.split("; ");
-        for draw in draws {
-            let mut colors = (0, 0, 0);
-            for color_draw in draw.split(", ") {
-                let (count, color) = color_draw.split_once(' ').unwrap();
-                let n = count.parse::<u32>().unwrap();
-                match color {
-                    "red" => colors.0 += n,
-                    "green" => colors.1 += n,
-                    "blue" => colors.2 += n,
-                    _ => unreachable!(),
-                }
-            }
-            min_colors.0 = max(min_colors.0, colors.0);
-            min_colors.1 = max(min_colors.1, colors.1);
-            min_colors.2 = max(min_colors.2, colors.2);
-        }
-        acc += min_colors.0 * min_colors.1 * min_colors.2;
+    let line_len = input
+        .iter()
+        .enumerate()
+        .find(|(_, &b)| b == b'\n')
+        .unwrap()
+        .0;
+
+    let plane = input
+        .iter()
+        .cloned()
+        .filter(|&b| b != b'\n')
+        .collect::<Vec<u8>>();
+
+    let mut positions: BTreeSet<usize> = BTreeSet::new();
+    // Find all symbols, and collect all surrounding leftmost-number fragments
+    for (pos, _) in plane
+        .iter()
+        .enumerate()
+        .filter(|(_, &b)| !(b as char).is_numeric() && b != b'.')
+    {
+        find_numeric(pos, &plane, line_len, &mut positions);
     }
+
+    let acc: usize = positions
+        .into_iter()
+        .map(|pos| expand_right_and_collect_numeric(pos, &plane, line_len))
+        .sum();
+
     dbg!(acc);
 }
