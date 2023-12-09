56f65ba25344a36907f856f0b5757e23bb5a00c0
melvyn <melvyn2@dnsense.pub>
2023-12-08 20:12:02 -0800

    Day 6 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,50 @@
-use std::ops::Range;
-
-use rayon::prelude::*;
-
 fn main() {
-    let input = include_str!("input.txt");
+    let input = include_str!("input.txt").trim().split_once('\n').unwrap();
 
-    let mut sec = input.split("\n\n");
-
-    let seeds = sec
-        .next()
-        .unwrap()
+    let times = input
+        .0
         .split_once(": ")
         .unwrap()
         .1
         .split(' ')
-        .map(|s| s.parse().unwrap())
-        .collect::<Vec<u64>>()
-        .chunks(2)
-        .map(|s| s[0]..s[0] + s[1])
-        .collect::<Vec<Range<u64>>>();
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<u64>().unwrap());
+    let records = input
+        .1
+        .split_once(": ")
+        .unwrap()
+        .1
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse::<u64>().unwrap());
 
-    let maps = sec
-        .map(|m| {
-            m.lines()
-                .skip(1)
-                .map(|l| {
-                    let v = l
-                        .split(' ')
-                        .map(|n| n.parse::<u64>().unwrap())
-                        .collect::<Vec<u64>>();
-                    (v[1]..v[1] + v[2], (v[0] as i64 - v[1] as i64))
-                })
-                .collect::<Vec<(Range<u64>, i64)>>()
-        })
-        .collect::<Vec<Vec<(Range<u64>, i64)>>>();
+    let res = times
+        .zip(records)
+        .map(|(time, record)| {
+            // Within a time limit l, the distance traveled, where p <= l is the time of release of the button,
+            // is p(l - p) or lp - p^2. If the record distance is r, lp - p^2 = r means
+            // p^2 - lp + r = 0, and so p = (-l ± sqrt(l^2 - 4r))/2.
+            // We are only trying to find the range of solutions or p2-p1
+            let l = time as f64;
+            let r = record as f64;
+            let p_big = (l + (l.powf(2.0) - (4.0 * r)).sqrt()) / 2.0;
+            let p_small = (l - (l.powf(2.0) - (4.0 * r)).sqrt()) / 2.0;
 
-    let res = seeds
-        .into_iter()
-        .map(|seed_range| {
-            seed_range
-                .into_par_iter()
-                .map(|s| {
-                    let mut c = s as i64;
-                    for map in &maps {
-                        let offset = map
-                            .iter()
-                            .find_map(|(r, o)| {
-                                if r.contains(&(c as u64)) {
-                                    Some(*o)
-                                } else {
-                                    None
-                                }
-                            })
-                            .unwrap_or(0);
-                        c += offset;
-                    }
-                    c
-                })
-                .min()
-                .unwrap()
+            // If the record thresholds land on a whole millisecond, we can't use that interval
+            // and have to consider the adjacent qualifying times. Else just round towards the center.
+            // We only do this for p_big because chances are, if one root is whole, both are,
+            // and so you'd have (p_big - 1) - (p_small + 1) + 1
+            // = (p_big - 1) - p_small
+            // Otherwise, floor(p_big) - ceil(p_small) + 1 = floor(p_big) - floor(p_small)
+            let i_big = if p_big % 10.0 < 0.0001 {
+                p_big as u64 - 1
+            } else {
+                p_big as u64
+            };
+            let i_small = p_small.floor() as u64;
+            i_big - i_small
         })
-        .min()
-        .unwrap();
+        .product::<u64>();
 
     dbg!(res);
 }
