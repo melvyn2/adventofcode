d87ae636cfc822bf00a70773f7637d2bae54937a
melvyn <melvyn2@dnsense.pub>
2023-12-18 17:14:57 -0800

    Day 9 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,34 @@
-use std::collections::BTreeMap;
-use std::process::exit;
+#![feature(let_chains)]
+#![feature(iter_map_windows)]
 
 fn main() {
     let input = include_str!("input.txt");
 
-    let (dir_str, map_str) = input.split_once("\n\n").unwrap();
-    let directions = dir_str
-        .bytes()
-        .map(|b| match b {
-            b'L' => 0,
-            b'R' => 1,
-            _ => unreachable!(),
-        })
-        .collect::<Vec<usize>>();
-
-    let map = map_str
+    let value_histories = input
         .lines()
-        .map(|s| {
-            let (key, vals_str) = s.split_once(" = ").unwrap();
-            let vals = vals_str[1..vals_str.len() - 1].split_once(", ").unwrap();
-            (
-                key.as_bytes().try_into().unwrap(),
-                [
-                    vals.0.as_bytes().try_into().unwrap(),
-                    vals.1.as_bytes().try_into().unwrap(),
-                ],
-            )
+        .map(|l| {
+            l.split_whitespace()
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>()
         })
-        .collect::<BTreeMap<[u8; 3], [[u8; 3]; 2]>>();
+        .collect::<Vec<Vec<i64>>>();
 
-    let mut pos = b"AAA";
-    let mut step = 0;
-    while pos != b"ZZZ" {
-        let dir = directions[step % directions.len()];
-        pos = &map[pos][dir];
-        step += 1;
+    let mut acc = 0;
+    for hist in value_histories {
+        let mut inner = 0;
+        let mut diffs = hist.clone();
+
+        while diffs.iter().any(|&x| x != 0) {
+            inner += diffs.last().unwrap();
+            let new = diffs
+                .iter()
+                .map_windows(|[&x, &y]| y - x)
+                .collect::<Vec<i64>>();
+            diffs = new;
+        }
+
+        dbg!(inner);
+        acc += inner;
     }
-
-    dbg!(step);
+    dbg!(acc);
 }
